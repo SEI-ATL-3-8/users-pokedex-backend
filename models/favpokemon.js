f389993e252dced17fc905847b42a58b4eb62f70
pmacaluso3 <moxious.manoeuvre@gmail.com>
2021-04-26 17:48:24 -0400

    remove association

--- models/favpokemon.js
+++ models/favpokemon.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      models.favPokemon.belongsTo(models.user)
+      // define associations here
     }
   };
   favPokemon.init({
