bec57878e29421687f39f86d2d6d0a9a13e770a2
pmacaluso3 <moxious.manoeuvre@gmail.com>
2021-04-27 13:09:37 -0400

    scope favPokemons to users

--- controllers/favPokemonController.js
+++ controllers/favPokemonController.js
@@ -3,7 +3,11 @@ const favPokemonController = {}
 
 favPokemonController.index = async (req, res) => {
   try {
-    const favPokemon = await models.favPokemon.findAll()
+    const user = await models.user.findOne({
+      where: { id: req.headers.authorization }
+    })
+
+    const favPokemon = await user.getFavPokemons()
     res.json({ favPokemon })
     
   } catch (error) {
@@ -13,9 +17,21 @@ favPokemonController.index = async (req, res) => {
 
 favPokemonController.create = async (req, res) => {
   try {
+    const user = await models.user.findOne({
+      where: { id: req.headers.authorization }
+    })
+
+    if (user === null) {
+      res.status(404).json({ message: 'user not found' })
+      return
+    }
+    
     const newFavPokemon = await models.favPokemon.create({
       name: req.body.name
     })
+
+    await user.addFavPokemon(newFavPokemon)
+
     res.json({ newFavPokemon })
   } catch (error) {
     res.status(400).json({ error: error.message })
@@ -24,9 +40,17 @@ favPokemonController.create = async (req, res) => {
 
 favPokemonController.destroy = async (req, res) => {
   try {
-    const deleteResult = await models.favPokemon.destroy({
-      where: { id: req.params.id }
+    const user = await models.user.findOne({
+      where: { id: req.headers.authorization }
     })
+
+    const deleteResult = await models.favPokemon.destroy({
+      where: {
+        name: req.params.name,
+        userId: user.id
+      }
+    })
+
     res.json({ deleteResult })
   } catch (error) {
     res.status(400).json({ error: error.message })
--- routes/favPokemonRoutes.js
+++ routes/favPokemonRoutes.js
@@ -3,6 +3,6 @@ const favPokemonController = require('../controllers/favPokemonController')
 
 favPokemonRoutes.get('/', favPokemonController.index)
 favPokemonRoutes.post('/', favPokemonController.create)
-favPokemonRoutes.delete('/:id', favPokemonController.destroy)
+favPokemonRoutes.delete('/:name', favPokemonController.destroy)
 
 module.exports = favPokemonRoutes
