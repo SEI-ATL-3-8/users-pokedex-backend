9e7226cae3f1e5e765d309645dcd627f83923bca
rz4life <rzalagbada@Abdulrazaqs-Air.cable.rcn.com>
2021-04-27 16:38:25 -0500

    rzhw

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+const models = require('./models')
 app.use(require('morgan')('tiny'))
 const routesReport = require('rowdy-logger').begin(app)
 
@@ -10,6 +10,33 @@ app.use(require('cors')())
 
 const favPokemonRoutes = require('./routes/favPokemonRoutes')
 app.use('/favPokemon', favPokemonRoutes)
+
+app.post('/users', (req, res) => {
+  models.user.create({
+    email: req.body.email,
+    password: req.body.password
+  })
+  .then((user) => {
+    res.json({ message: 'success', user})
+  })
+  .catch((error) => {
+    res.status(400).json({ error: error.message })
+  })
+})
+
+app.post('/users/login', (req, res) => {
+  models.user.findOne({
+    where: { email: req.body.email }
+  }).then((foundUser) => {
+    if (foundUser && foundUser.password === req.body.password) {
+      res.json({ message: 'success', user: foundUser })
+    } else {
+      res.status(401).json({ message: 'login failed' })
+    }
+  }).catch((error) => {
+    res.status(400).json({ error: error.message })
+  })
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
